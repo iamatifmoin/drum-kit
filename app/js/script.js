1f1c8cf96cb3f96196f24c54905df89e802bffaf
iamatifmoin <atifmoin3@gmail.com>
2022-08-15 12:42:49 +0530

    organised into folders

---
+++ app/js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener( 'keydown', function ( e )
+{
+    const audio = document.querySelector( `audio[data-key="${ e.keyCode }"]` );
+    const key = document.querySelector( `.key[data-key="${ e.keyCode }"]` );
+    if ( !audio )
+        return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add( 'playing' );
+    setTimeout( function ( e )
+    {
+        key.classList.remove( 'playing' );
+
+    }, 250 )
+} );
